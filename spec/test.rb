dddf5a542e542177577680f6e1d6f6a052efab5a
開発者のきよ <devnokiyo@gmail.com>
2018-12-16 16:33:44 +0900

    配列の長さの確認方法をempty?メソッドへ変更

--- spec/test.rb
+++ spec/test.rb
@@ -65,11 +65,11 @@ end
 def dialog_title
   if ios?
     xpath = '//XCUIElementTypeStaticText[@name="認証エラー"]'
-    return nil if find_elements(:xpath, xpath).size == 0
+    return nil if find_elements(:xpath, xpath).empty?
     find_element(:xpath, xpath).value
   else
     id = 'alertTitle'
-    return nil if find_elements(:id, id).size == 0
+    return nil if find_elements(:id, id).empty?
     find_element(:id, id).text
   end
 end
@@ -81,11 +81,11 @@ end
 def dialog_message
   if ios?
     id = 'IDまたはパスワードが違います。'
-    return nil if find_elements(:id, id).size == 0
+    return nil if find_elements(:id, id).empty?
     find_element(:id, id).value
   else
     id = 'message'
-    return nil if find_elements(:id, id).size == 0
+    return nil if find_elements(:id, id).empty?
     find_element(:id, id).text
   end
 end
@@ -105,6 +105,6 @@ end
 # @return [String] ラベルのタイトル
 # @return [nil] ラベルが存在しない場合はnil
 def label_title
-  return nil if find_elements(:id, 'lblTitle').size == 0
+  return nil if find_elements(:id, 'lblTitle').empty?
   find_element(:id, 'lblTitle').text
 end
